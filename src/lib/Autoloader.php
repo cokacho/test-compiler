7fcbd86cc326a0de281d595ff0271dabe5e7c4dd
cokacho@gmail.com <mierdota123>
2015-12-01 16:07:43 -0400

    Add composer.json

---
+++ src/lib/Autoloader.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bad_f
+ * Date: 01/12/2015
+ * Time: 16:05
+ */
+class Autoloader
+{
+    public static function start(){
+        echo "Hello world";
+    }
+}
